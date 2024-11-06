8df89b25bb7f0f873ed4d58d612f5fbe7177e0c1
lilsaad <saadinbusiness3@gmail.com>
2024-11-06 14:31:54 +0530

    Add Null Feild State

--- app/src/main/java/com/example/nurseflowd1/screens/nurseauth/UserRegisPage.kt
+++ app/src/main/java/com/example/nurseflowd1/screens/nurseauth/UserRegisPage.kt
@@ -1,6 +1,5 @@
 package com.example.nurseflowd1.screens.nurseauth
 
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,16 +19,15 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -44,20 +42,20 @@ import com.example.nurseflowd1.ui.theme.jersery25
 @Composable
 fun NurseRegister(modifier: Modifier = Modifier, navController: NavController , viewmodel : AppVM){
     @Composable
-    fun ScreenWidhth(k : Double ) : Double = (LocalConfiguration.current.screenWidthDp * k)
+    fun ScreenWidth(k : Double ) : Double = (LocalConfiguration.current.screenWidthDp * k)
 
-    var user_name  = remember { mutableStateOf("") }
-    var user_surname  = remember { mutableStateOf("") }
-    var hospital_name  = remember { mutableStateOf("") }
-    var hospital_id  = remember { mutableStateOf("") }
-    var nurse_license_id  = remember { mutableStateOf("") }
-    var gender = remember { mutableStateOf("") }
-    var age = remember { mutableStateOf("") }
+    var user_name  = remember { mutableStateOf("") } ; var username_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var user_surname  = remember { mutableStateOf("") } ; var usersurname_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var hospital_name  = remember { mutableStateOf("") }; var hospitalname_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var hospital_id  = remember { mutableStateOf("") }; var hospitalid_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var nurse_license_id  = remember { mutableStateOf("") }; var nurselicense_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var gender = remember { mutableStateOf("") }; var gender_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var age = remember { mutableStateOf("") }; var age_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
 
     Column( modifier = modifier.fillMaxSize().background(AppBg).verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally){
 
-        Column(modifier = Modifier.padding(top = ScreenWidhth(0.3).dp )
+        Column(modifier = Modifier.padding(top = ScreenWidth(0.3).dp )
             .fillMaxWidth(0.9f).fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -66,87 +64,115 @@ fun NurseRegister(modifier: Modifier = Modifier, navController: NavController , 
             SingupFeilds(
                 label = "Name",
                 textstate = user_name,
-                placeholdertext = "Enter your firstname"
+                placeholdertext = "Enter your firstname",
+                username_ststate
             )
             SingupFeilds(
                 label = "Surname",
                 textstate = user_surname,
-                placeholdertext = "Enter your lastname"
+                placeholdertext = "Enter your lastname",
+                usersurname_ststate
             )
             SingupFeilds(
                 label = "Hospital",
                 textstate = hospital_name,
-                placeholdertext = "Enter your hospital name"
+                placeholdertext = "Enter your hospital name",
+                hospitalname_ststate
             )
             SingupFeilds(
                 label = "Hospital ID",
                 textstate = hospital_id,
-                placeholdertext = "Enter Id provided by your hospital"
+                placeholdertext = "Enter Id provided by your hospital",
+                hospitalid_ststate
             )
             SingupFeilds(
                 label = "License ID",
                 textstate = nurse_license_id,
-                placeholdertext = "Enter your Unique License id provided by Nursing Body"
+                placeholdertext = "Enter your Unique License id provided by Nursing Body",
+                nurselicense_ststate
             )
 
-            val softwarekeyboard = GiveKeyboard()
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
+
+            Row(modifier = Modifier.fillMaxWidth().padding( horizontal = ScreenWidth(0.2).dp)
             ){
-                OutlinedTextField( value = gender.value , onValueChange = {
-                        it -> gender.value = it
-                },
-                    placeholder = { Text("Gender" , color = Color.White.copy(0.50f) )},
-                    modifier = Modifier.fillMaxWidth(0.45f),
-                    keyboardOptions = KeyboardOptions(
-                        imeAction = ImeAction.Done
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            softwarekeyboard.hide()
-                        }
-                    )
-                )
-                Spacer(modifier = Modifier.size( ScreenWidhth(0.02).dp ))
-                OutlinedTextField( value = age.value , onValueChange = {
-                        it -> age.value = it
-                },
-                    placeholder = { Text("Age" , color = Color.White.copy(0.50f) )},
-                    modifier = Modifier.fillMaxWidth(0.45f),
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number,
-                        imeAction = ImeAction.Done
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            softwarekeyboard.hide()
-                        }
-                    )
-                )
-
+                SignupFeildsSecond(modifier = Modifier.weight(1f), gender , gender_ststate , "Gender")
+                Spacer( modifier = Modifier.size(ScreenWidth(0.1).dp))
+                SignupFeildsSecond(modifier = Modifier.weight(0.6f), age , age_ststate , "Age")
             }
-            val context = LocalContext.current
             Button( onClick = {
-                try {
-                    val nurseinfo = NurseRegisInfo( N_name = user_name.value , N_surname =  user_surname.value ,
-                        N_hospitalname =  hospital_name.value, N_hospitalid = hospital_id.value ,
-                        N_license_id = nurse_license_id.value , N_gender = gender.value , N_age = age.value.toInt()
-                    )
-                    viewmodel.SaveNurseInfo(nurseinfo)
-                    navController.navigate( route = Destinations.AuthScreen.ref)
-                }catch (e : Exception){
-                    // Handle Button Clicked with any values Entered
-                    Toast.makeText(context,"the error is $e" , Toast.LENGTH_LONG).show()
-                }
+                    fun NotEmptyFeilds() : Boolean { var isvalid = true
+                        if( user_name.value.isBlank()){ username_ststate.value = SupportTextState.empty("Required*") ; isvalid = false}
+                        else { username_ststate.value = SupportTextState.ideal }
 
+                        if( user_surname.value.isBlank()){ usersurname_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                        else { usersurname_ststate.value = SupportTextState.ideal }
+
+                        if( hospital_name.value.isBlank()){ hospitalname_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                        else { hospitalname_ststate.value = SupportTextState.ideal }
+
+                        if( hospital_id.value.isBlank()){ hospitalid_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                        else {hospitalid_ststate.value = SupportTextState.ideal }
+
+                        if( nurse_license_id.value.isBlank()){ nurselicense_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                        else { nurselicense_ststate.value = SupportTextState.ideal }
+
+                        if(gender.value.isBlank()){gender_ststate.value = SupportTextState.empty("Required") ; isvalid = false}
+                        else { gender_ststate.value = SupportTextState.ideal }
+
+                        if(age.value.isBlank()){age_ststate.value = SupportTextState.empty("Required") ; isvalid = false}
+                        else { age_ststate.value = SupportTextState.ideal }
+
+                        return isvalid
+                    }
+                    if (NotEmptyFeilds()){
+                        val nurseinfo = NurseRegisInfo( N_name = user_name.value , N_surname =  user_surname.value ,
+                            N_hospitalname =  hospital_name.value, N_hospitalid = hospital_id.value ,
+                            N_license_id = nurse_license_id.value , N_gender = gender.value , N_age = age.value.toInt()
+                        )
+                        viewmodel.SaveNurseInfo(nurseinfo)
+                        navController.navigate( route = Destinations.AuthScreen.ref)
+                    }
             },
                 colors = ButtonColors( containerColor = HTextClr , contentColor = Color.White , disabledContentColor = Color.Black , disabledContainerColor = Color.White ),
-                modifier = Modifier.padding(top = ScreenWidhth (0.04).dp ).size(width = ScreenWidhth(0.50).dp , height = ScreenWidhth(0.11) .dp)
+                modifier = Modifier.padding(top = ScreenWidth (0.04).dp ).size(width = ScreenWidth(0.50).dp , height = ScreenWidth(0.11) .dp)
             ) {
-                Text("Finish" , fontFamily = jersery25 , fontWeight = FontWeight.Bold , fontSize = ScreenWidhth (0.05).sp)
+                Text("Finish" , fontFamily = jersery25 , fontWeight = FontWeight.Bold , fontSize = ScreenWidth (0.05).sp)
             }
         }
 
 
     }
 }
+@Composable
+fun SignupFeildsSecond( modifier: Modifier,
+    textstate : MutableState<String> , supporttextstate : MutableState<SupportTextState> ,
+    placeholder : String
+){
+    val iserror = remember { mutableStateOf(false) }
+    @Composable
+    fun ScreenWidth(k : Double ) : Double = (LocalConfiguration.current.screenWidthDp * k)
+    val softwarekeyboard = GiveKeyboard()
+        OutlinedTextField( value = textstate.value , onValueChange = {
+                it -> textstate.value = it
+        },
+            placeholder = { Text(placeholder , color = Color.White.copy(0.50f) )},
+            modifier = modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    softwarekeyboard.hide()
+                }
+            ),
+            supportingText = {
+                when(val state = supporttextstate.value) {
+                    is SupportTextState.empty ->  { Text(state.errormsg) ; iserror.value = true}
+                    is SupportTextState.ideal -> { iserror.value = false }
+                    else -> Unit
+                }
+            },
+            isError = iserror.value
+        )
+}
+
--- app/src/main/java/com/example/nurseflowd1/screens/patientboarding/PaitentRegisPage.kt
+++ app/src/main/java/com/example/nurseflowd1/screens/patientboarding/PaitentRegisPage.kt
@@ -14,9 +14,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -34,27 +34,27 @@ import com.example.nurseflowd1.ui.theme.HTextClr
 import com.example.nurseflowd1.ui.theme.jersery25
 import com.example.nurseflowd1.datamodels.PatientInfo
 import com.example.nurseflowd1.screens.Destinations
-
+import com.example.nurseflowd1.screens.nurseauth.SignupFeildsSecond
+import com.example.nurseflowd1.screens.nurseauth.SupportTextState
 
 
 @Composable
 fun Paitent_Regis_Screen( modifier: Modifier = Modifier , navcontroller : NavController , viewmodel: AppVM){
     @Composable
-    fun ScreenWidhth(k : Double ) : Double = (LocalConfiguration.current.screenWidthDp * k)
+    fun ScreenWidth(k : Double ) : Double = (LocalConfiguration.current.screenWidthDp * k)
 
-    var user_name  = remember { mutableStateOf("") }
-    var user_surname  = remember { mutableStateOf("") }
-    var phoneno  = remember { mutableStateOf("") }
-    var kin = remember { mutableStateOf("") }
-    var patient_id = remember { mutableStateOf("") }
-    var doctorname = remember { mutableStateOf("") }
-    var gender = remember { mutableStateOf("") }
-    var age = remember { mutableStateOf("") }
+    var user_name  = remember { mutableStateOf("") } ; var username_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var user_surname  = remember { mutableStateOf("") }; var usersurname_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var phoneno  = remember { mutableStateOf("") }; var phoneno_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var kin = remember { mutableStateOf("") }; var kin_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var patient_id = remember { mutableStateOf("") }; var patientid_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var doctorname = remember { mutableStateOf("") }; var doctorname_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var gender = remember { mutableStateOf("") }; var gender_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
+    var age = remember { mutableStateOf("") }; var age_ststate : MutableState<SupportTextState> = remember { mutableStateOf(SupportTextState.ideal) }
 
     Column( modifier = modifier.fillMaxSize().background(AppBg).verticalScroll( rememberScrollState() ) ,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
-
 
         Column(modifier = Modifier
             .fillMaxWidth(0.9f).fillMaxHeight(),
@@ -64,65 +64,87 @@ fun Paitent_Regis_Screen( modifier: Modifier = Modifier , navcontroller : NavCon
             SingupFeilds(
                 label = "Name",
                 textstate = user_name,
-                placeholdertext = "Enter paitent name"
+                placeholdertext = "Enter paitent name",
+                username_ststate
             )
             SingupFeilds(
                 label = "Surname",
                 textstate = user_surname,
-                placeholdertext = "Enter patient surname"
+                placeholdertext = "Enter patient surname",
+                usersurname_ststate
             )
             SingupFeilds(
                 label = "Phone no",
                 textstate = phoneno,
-                placeholdertext = "Enter patient phone number "
+                placeholdertext = "Enter patient phone number ",
+                phoneno_ststate
             )
             SingupFeilds(
                 label = "Kin",
                 textstate = kin,
-                placeholdertext = "Enter kin full name "
+                placeholdertext = "Enter kin full name ",
+                kin_ststate
             )
             SingupFeilds(
                 label = "Paitent ID",
                 textstate = patient_id,
-                placeholdertext = "Enter paitent id assinged by your hosptial  "
+                placeholdertext = "Enter paitent id assinged by your hosptial  ",
+                patientid_ststate
             )
             SingupFeilds(
                 label = "Doctor",
                 textstate = doctorname,
-                placeholdertext = "Enter the assgined Doctor"
+                placeholdertext = "Enter the assgined Doctor",
+                doctorname_ststate
             )
-
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
+            Row(modifier = Modifier.fillMaxWidth().padding( horizontal = ScreenWidth(0.2).dp)
             ){
-                OutlinedTextField( value = gender.value , onValueChange = {
-                        it -> gender.value = it
-                },
-                    placeholder = { Text("Gender" , color = Color.White.copy(0.50f) )},
-                    modifier = Modifier.fillMaxWidth(0.45f)
-                )
-                Spacer(modifier = Modifier.size( ScreenWidhth(0.02).dp ))
-                OutlinedTextField( value = age.value , onValueChange = {
-                        it -> age.value = it
-                },
-                    placeholder = { Text("Age" , color = Color.White.copy(0.50f) )},
-                    modifier = Modifier.fillMaxWidth(0.45f)
-                )
+                SignupFeildsSecond(modifier = Modifier.weight(1f), gender , gender_ststate , "Gender")
+                Spacer( modifier = Modifier.size(ScreenWidth(0.1).dp))
+                SignupFeildsSecond(modifier = Modifier.weight(0.6f), age , age_ststate , "Age")
             }
             Button( onClick = {
-                val patientinfo = PatientInfo(
-                    p_name =  user_name.value, p_surename = user_surname.value , p_phoneno = phoneno.value,
-                    p_patientid = patient_id.value ,
-                    p_doctor = doctorname.value , p_age = age.value , p_gender = gender.value
-                )
+                fun NotEmptyFeilds() : Boolean { var isvalid = true
+                    if( user_name.value.isBlank()){ username_ststate.value = SupportTextState.empty("Required*") ; isvalid = false}
+                    else { username_ststate.value = SupportTextState.ideal }
+
+                    if( user_surname.value.isBlank()){ usersurname_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                    else { usersurname_ststate.value = SupportTextState.ideal }
+
+                    if( phoneno.value.isBlank()){ phoneno_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                    else { phoneno_ststate.value = SupportTextState.ideal }
+
+                    if( kin.value.isBlank()){ kin_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                    else { kin_ststate.value = SupportTextState.ideal }
+
+                    if( patient_id.value.isBlank()){ patientid_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                    else { patientid_ststate.value = SupportTextState.ideal }
+
+                    if( doctorname.value.isBlank()){ doctorname_ststate.value = SupportTextState.empty("Required*")  ; isvalid = false}
+                    else { doctorname_ststate.value = SupportTextState.ideal }
+
+                    if(gender.value.isBlank()){gender_ststate.value = SupportTextState.empty("Required") ; isvalid = false}
+                    else { gender_ststate.value = SupportTextState.ideal }
+
+                    if(age.value.isBlank()){age_ststate.value = SupportTextState.empty("Required") ; isvalid = false}
+                    else { age_ststate.value = SupportTextState.ideal }
+
+                    return isvalid
+                }
+                if(NotEmptyFeilds()){
+                    val patientinfo = PatientInfo(
+                        p_name =  user_name.value, p_surename = user_surname.value , p_phoneno = phoneno.value,
+                        p_patientid = patient_id.value ,
+                        p_doctor = doctorname.value , p_age = age.value , p_gender = gender.value
+                    )
                     viewmodel.SavePatientInfoFirestore(patientinfo)
                     navcontroller.popBackStack(Destinations.NurseDboardScreen.ref , inclusive = false)
-
+                }
             },
                 colors = ButtonColors( containerColor = HTextClr , contentColor = Color.White , disabledContentColor = Color.Black , disabledContainerColor = Color.White ),
-                modifier = Modifier.padding(top = ScreenWidhth (0.04).dp ).size(width = ScreenWidhth(0.50).dp , height = ScreenWidhth(0.11) .dp)
+                modifier = Modifier.padding(top = ScreenWidth (0.04).dp ).size(width = ScreenWidth(0.50).dp , height = ScreenWidth(0.11) .dp)
             ){
-                Text("Continue -->" , fontFamily = jersery25 , fontWeight = FontWeight.Bold , fontSize = ScreenWidhth (0.05).sp)
+                Text("Continue -->" , fontFamily = jersery25 , fontWeight = FontWeight.Bold , fontSize = ScreenWidth (0.05).sp)
             }
         }
 
